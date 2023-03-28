bc148d41220090158538b1136870df98d3c78153
Linet Oliech <linetaoliech@gmail.com>
2023-03-28 21:01:14 +0300

    added validations in the campaign model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,12 @@
 class Campaign < ApplicationRecord
-    #added associations 
-
+    #validations 
+    validates :title, presence: true
+    validates :description, presence: true, length: {maximum: 500}
+    validates :goal_amount, presence: true, numericality: {greater_than:0}
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+    
+    #associations 
     belongs_to :user
     has_many :reviews, through: :user
     has_many :pledges 
