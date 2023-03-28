e5248b8f25a717b2d56f6730d340e38143b50dbf
Linet Oliech <linetaoliech@gmail.com>
2023-03-28 22:08:26 +0300

    updated validation for campaign category

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,13 +1,12 @@
 class Campaign < ApplicationRecord
     #validations 
+    CATEGORIES = ['Education', 'IT', 'Hospitality', 'Farming', 'Advertising', 'Manufacturing', 'Healthcare', 'Legal']
+
     validates :title, presence: true
     validates :description, presence: true, length: {maximum: 500}
     validates :goal_amount, presence: true, numericality: {greater_than:0}
     validates :start_date, presence: true
     validates :end_date, presence: true
-    
-    CATEGORIES = ['Education', 'IT', 'Hospitality', 'Farming', 'Advertising', 'Manufacturing', 'Healthcare', 'Legal']
-
     validates :category, inclusion: {
     in: CATEGORIES,
     message: "must be one of: #{CATEGORIES.join(', ')}"
