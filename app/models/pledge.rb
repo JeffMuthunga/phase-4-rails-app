13e470ebb01493a5a3e4b44a155c6b9e136de331
Linet Oliech <linetaoliech@gmail.com>
2023-03-28 21:19:13 +0300

    added validation to pledge model

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,7 @@
 class Pledge < ApplicationRecord
+    #validations
+    validates :pledge_amount, presence: true, numericality: {greater_than: 0}
+
     #associations
     belongs_to :user
     belongs_to :campaign  
