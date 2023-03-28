9468340f4153c0797fe9f4cf0c925f6228881b5a
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-03-28 20:01:28 +0300

    Campaign details

--- client/src/components/common/CardDetails/CardDetails.jsx
+++ client/src/components/common/CardDetails/CardDetails.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles from './CardDetails.module.css'
+import oldman from './old_man_campaign.jpg'
 
 function CardDetails() {
   return (
-    <div>CardDetails</div>
+    <div className={styles.body_container}>
+    <div className={styles.container}>
+        <h1>Hungama</h1>
+        <h2>Project by Nawal Mubin</h2>
+        <h3>Category: War</h3>
+        <div className={styles.photo_container}>
+            <img src={oldman} alt="Campaign image" />
+        </div>
+        <div className={styles.details_container}>
+
+        </div>
+
+    </div>
+    </div>
   )
 }
 
