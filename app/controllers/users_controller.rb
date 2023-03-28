5cabe79e0becf97f3b9d6543ceb1cf0308cb8cb9
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-03-29 00:12:01 +0300

    creation of user create action method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
     
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
     
 end
