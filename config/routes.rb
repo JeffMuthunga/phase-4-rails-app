d895378e8d3194881bcdf68c0af97c42ece5b392
hassan kazungu <hassankazungu01@gmail.com>
2023-03-29 16:09:17 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :reviews
   resources :pledges
   resources :campaigns
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
