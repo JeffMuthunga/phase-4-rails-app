a70d5b090eeffbc8860637dd28dce7eaf6ba9af0
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-03-29 04:52:28 +0300

    Card details complete

--- client/src/components/common/CardDetails/CardDetails.jsx
+++ client/src/components/common/CardDetails/CardDetails.jsx
@@ -1,23 +1,39 @@
 import React from 'react'
 import styles from './CardDetails.module.css'
-import oldman from './old_man_campaign.jpg'
+import oldman from './h.jpg'
 
 function CardDetails() {
   return (
     <div className={styles.body_container}>
-    <div className={styles.container}>
         <h1>Hungama</h1>
         <h2>Project by Nawal Mubin</h2>
         <h3>Category: War</h3>
+    <div className={styles.container}>
         <div className={styles.photo_container}>
             <img src={oldman} alt="Campaign image" />
         </div>
         <div className={styles.details_container}>
-
+        <div className={styles.separator}>
+            <h1>sibasid</h1>
+            <hr className={styles.line}/>
+        </div>
+        aksfk
+        
         </div>
 
     </div>
+    <div className={styles.input_container}>
+    <form >
+        <label htmlFor="fname"> Leave a Review</label>
+        <div className={styles.input_group}>
+        <input type="text"  name="comment" placeholder="Leave a Review..."/>     
+        <input type="number"  name="star_rating" placeholder="Stars"/>       
+        <input type="submit"  value="Post Review"/>
+        </div>
+    </form>
     </div>
+    </div>
+
   )
 }
 
