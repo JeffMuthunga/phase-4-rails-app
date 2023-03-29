a9500d66234bcb4eef4a86853908d0382c9956ee
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-03-29 12:34:02 +0300

    updating DB

--- db/migrate/20230328174727_create_campaigns.rb
+++ db/migrate/20230328174727_create_campaigns.rb
@@ -5,10 +5,10 @@ class CreateCampaigns < ActiveRecord::Migration[7.0]
       t.string :description
       t.string :category
       t.float :goal_amount
+      t.integer :current_amount
       t.integer :start_date
       t.integer :end_date
       t.integer :user_id
-      t.integer :current_amount
 
       t.timestamps
     end
