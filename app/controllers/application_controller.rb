8bd3acace20df2b7dc0b9ff1bd1c98d54b64b3ec
hassan kazungu <hassankazungu01@gmail.com>
2023-03-29 15:48:19 +0300

    configure errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+    wrap_parameters format: []
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_message
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 end
