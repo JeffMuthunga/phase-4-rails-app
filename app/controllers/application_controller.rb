ef3087420919da442f461b4ac135ac70b90480b6
hassan kazungu <hassankazungu01@gmail.com>
2023-03-29 15:50:13 +0300

    added record invalid error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::API
     def render_not_found_message
         render json: {error: "#{controller_name.classify} not found"}, status: :not_found
     end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
 end
