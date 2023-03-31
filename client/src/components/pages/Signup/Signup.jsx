eeea28b77abd11a2ca219207f7fefa3c69a27bd5
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-04-01 00:43:20 +0300

    updated navbar and banner

--- client/src/components/pages/Signup/Signup.jsx
+++ client/src/components/pages/Signup/Signup.jsx
@@ -11,7 +11,6 @@ function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [change, setOnChange] = useState(false);
   const [errors, setErrors] = useState([])
 
   // sign up
