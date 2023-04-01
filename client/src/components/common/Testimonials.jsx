ea529b0521f010b93baaf3646067a3d2ff40e80a
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-04-01 15:05:01 +0300

    added testimonials to Home

--- client/src/components/common/Testimonials.jsx
+++ client/src/components/common/Testimonials.jsx
@@ -2,19 +2,39 @@ import React from 'react'
 
 function Testimonials() {
   return (
-    <div className='testimonials'>
-        <h1>Testimonials</h1>
-        <hr styles={{color: 'black'}}/>
-        <p>Campaigns that reached their goals</p>
-        <div className="cases">
-            <p>Run for Reading is a crowdfunding campaign for the Team First Book chapter in New Hanover County, North Carolina. The campaign was able to raise enough money to provide new books to children and schools in lower income neighborhoods.</p>
-            <img src="https://images.pexels.com/photos/14071907/pexels-photo-14071907.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Kids holding books"/>
-        </div>
-        <div className="cases">            
-            <img src="https://images.pexels.com/photos/663573/pexels-photo-663573.jpeg?auto=compress&cs=tinysrgb&w=400" alt="Cute dog" />
-            <p>The Dos Velas Images Saving Lives 1 Click at a Time crowdfunding campaign was created by Kelly Vela, a professional photographer who takes pictures of animals in shelters for free. The pictures she takes give animals a better chance at getting adopted.</p>
-        </div>
-    </div>
+        <div className="testimonial-img col-md mb-4 mt-4" style={{display : "flex"}} >
+          <div className="card w-100 text-center p-4 m-4">
+              <img src= "https://images.unsplash.com/photo-1522035612764-b0e4ba8915e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5NTg0NDQ1fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" 
+              alt="Nderitu's plane"
+              style={{objectFit: "cover"}} 
+              className="card-img-top"
+              height='400px'
+            />
+              <div className="card-body">
+                <h1 className="card-title mb-0">Nderitu's plane</h1> 
+              </div>           
+          </div>
+          <div className="card w-100 text-center p-4 m-4">            
+              <img src="https://images.unsplash.com/photo-1530884698386-d42ad3199b1f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" 
+              alt="Next in fashion design" 
+              style={{objectFit: "cover"}} 
+              className="card-img-top"
+              height='400px'/>
+              <div className="card-body">
+                <h1 className="card-title mb-0">Pink & Pearl Men's Wear</h1>
+              </div>
+          </div>
+          <div className="card w-100 text-center p-4 m-4">            
+              <img src="https://images.unsplash.com/photo-1519748771451-a94c596fad67?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="Woman in Tech startup" 
+              style={{objectFit: "cover"}} 
+              className="card-img-top"
+              height='400px'/>
+              <div className="card-body">
+                <h1 className="card-title mb-0">FullStack Web Designs by WoTech.</h1>
+              </div>
+          </div>
+      </div>
   )
 }
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,20 +2,23 @@ import React from 'react'
 import Banner from '../common/Banner'
 import Brands from '../common/Brands'
 import { NavLink } from 'react-router-dom'
-import CampaignItem from '../common/Card/CampaignItem'
+import Testimonials from '../common/Testimonials'
+
 
 
 
 function Home({search}) {
+
   return (
     <div className='home-container text-center'>   
       <Banner/>
       <h1>Find it First on Pledge Up</h1>
       <p>Pledge up is where early adopters and innovation seekers find lively, imaginative tech before it hits the market and where we you, can express your diverse interest </p>
-      <div>
+      <div style={{marginBottom: '3em'}}>
         <NavLink className= 'btn btn-warning btn-lg' to='/about'>Learn More</NavLink>
       </div>
-      
+      <h1>Testimonials</h1>
+      <Testimonials/>
       <Brands/> 
     </div>
   )
