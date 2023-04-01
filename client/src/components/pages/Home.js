25b13cc368566ac1defd9c39c819a95fa9a8e43b
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-04-01 15:28:57 +0300

    added custom routes and methods for our statistics

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,6 +3,7 @@ import Banner from '../common/Banner'
 import Brands from '../common/Brands'
 import { NavLink } from 'react-router-dom'
 import Testimonials from '../common/Testimonials'
+import Statistics from '../common/Statistics/Statistics'
 
 
 
@@ -12,6 +13,7 @@ function Home({search}) {
   return (
     <div className='home-container text-center'>   
       <Banner/>
+      <Statistics/>
       <h1>Find it First on Pledge Up</h1>
       <p>Pledge up is where early adopters and innovation seekers find lively, imaginative tech before it hits the market and where we you, can express your diverse interest </p>
       <div style={{marginBottom: '3em'}}>
@@ -19,7 +21,8 @@ function Home({search}) {
       </div>
       <h1>Testimonials</h1>
       <Testimonials/>
-      <Brands/> 
+     
+      {/* <Brands/>  */}
     </div>
   )
 }
