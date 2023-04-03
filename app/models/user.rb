3a4cd1ae66b785b2669a6b77c0fa1e0b7b5e23ef
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-04-03 07:30:39 +0300

    User validation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
 
     validates :username, presence: true,length: { minimum: 4 },  uniqueness: true
     validates :email, presence: true,  uniqueness: true
-    # validates_format_of :password, with: /\A(?=.*[A-Z])(?=.*\d).{8,}\z/, message: "must be at least 8 characters long and include one uppercase letter and one digit"
-    # validates :password_confirmation, presence: true
+    validates_format_of :password, with: /\A(?=.*[A-Z])(?=.*\d).{8,}\z/, message: "must be at least 8 characters long and include one uppercase letter and one digit"
+    validates :password_confirmation, presence: true
 end
